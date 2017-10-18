7371c05115db3f9947eddd2c61a9d974ad4e2c32
Julianne Kim <juliannne@gmail.com>
2017-10-18 22:18:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 def line(katz_deli)
+  str = "The line is currently: "
   if katz_deli.empty?
     puts "The line is currently empty."
   end
+  katz_deli.each_with_index do |name, idx|
+    str << "#{idx + 1}. #{name}"
+  end
+  str
 end
